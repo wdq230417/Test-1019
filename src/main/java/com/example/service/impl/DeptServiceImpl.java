187eb5c2dfc19fc8583d991e4af3c34156366e6b
86152 <https://github.com/wdq230417/wdq.git>
2022-10-19 17:14:53 +0800

    创建部门

---
+++ src/main/java/com/example/service/impl/DeptServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.service.impl;
+
+import com.example.service.DeptService;
+
+public class DeptServiceImpl implements DeptService {
+    @Override
+    public void createDept() {
+        System.out.println("创建部门");
+    }
+}
