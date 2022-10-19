6a8ff7ff62d308f89cdf4ee54b0d339be322e8c9
86152 <https://github.com/wdq230417/wdq.git>
2022-10-19 17:22:19 +0800

    第一次入职

---
+++ src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.service.impl;
+
+import com.example.service.DeptService;
+import com.example.service.EmpService;
+
+public class EmpServiceImpl implements EmpService {
+    @Override
+    public void createEmp() {
+        System.out.println("入职");
+    }
+}
